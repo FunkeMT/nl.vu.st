510fc27b5af82b8c7e8e01c0ead315efe58c05eb
pjotr <pjotrscholtze1@gmail.com>
2020-04-20 20:54:37 +0200

    Personal information should be given as command line arguments #8

--- tests/test_argv.py
+++ tests/test_argv.py
@@ -4,13 +4,11 @@ from heartmonitor import argv_parser
 
 
 def test_argv_parse_1(capsys):
-    """Correct name argument and printing"""
     res = argv_parser.parse(["--test", "hello"], "--test")
     assert "hello" == res
 
 
 def test_argv_parse_2(capsys):
-    """Correct name argument and printing"""
     happend = False
     try:
         argv_parser.parse(["--test", "hello"], "--test2")
@@ -19,7 +17,6 @@ def test_argv_parse_2(capsys):
     assert happend
 
 def test_argv_parse_3(capsys):
-    """Correct name argument and printing"""
     happend = False
     try:
         argv_parser.parse([], "--test2")
@@ -28,7 +25,6 @@ def test_argv_parse_3(capsys):
     assert happend
 
 def test_argv_parse_4(capsys):
-    """Correct name argument and printing"""
     happend = False
     try:
         argv_parser.parse([], "")
@@ -37,7 +33,6 @@ def test_argv_parse_4(capsys):
     assert happend
 
 def test_argv_parse_5(capsys):
-    """Correct name argument and printing"""
     happend = False
     try:
         argv_parser.parse(["--test"], "--test")
