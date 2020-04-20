de25ac7d39ab865997ad57a00155772bf4474f73
pjotr <pjotrscholtze1@gmail.com>
2020-04-20 20:47:08 +0200

    Personal information should be given as command line arguments #8

--- heartmonitor/argv_parser.py
+++ heartmonitor/argv_parser.py
@@ -1,7 +1,9 @@
-from typing import List
+from typing import List, Dict
 
-def parse(argv: List[str], key: str):
+def parse(argv: List[str], key: str) -> str:
     """
+    Parses argv commands for key value pair.
+
     :param: argv: Commandline arguments.
     :param: key: Key to find in the commandline arguments.
     :return: Value of the commandline argument.
@@ -16,3 +18,21 @@ def parse(argv: List[str], key: str):
     if valueIndex >= len(argv):
         raise ValueError
     return argv[valueIndex]
+
+
+def parse_multiple(argv: List[str], keys: List[str]) -> Dict[str, str]:
+    """
+    Parses argv commands for key value pairs.
+
+    :param: argv: Commandline arguments.
+    :param: keys: Keys to find in the commandline arguments.
+    :return: Per key the value of the commandline argument.
+    :raises: NameError When key was not found.
+    :raises: ValueError When value was not found after key.
+    """
+    res = {}
+    for key in keys:
+        res[key] = parse(argv, key)
+
+    return res
+
