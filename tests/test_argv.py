9b73d1e57e4f9775976cddb883c17acbe0bf37a1
pjotr <pjotrscholtze1@gmail.com>
2020-04-20 20:46:06 +0200

    Personal information should be given as command line arguments #8

--- tests/test_argv.py
+++ tests/test_argv.py
@@ -44,3 +44,34 @@ def test_argv_parse_5(capsys):
     except ValueError:
         happend = True
     assert happend
+
+
+def test_argv_parse_multiple(capsys):
+    res = argv_parser.parse_multiple(["--test", "hi", "--key","world"], ["--test", "--key"])
+    assert "--test" in res
+    assert "--key" in res
+    assert res["--test"] == "hi"
+    assert res["--key"] == "world"
+    assert len(res.keys()) == 2
+
+def test_argv_parse_multiple_2(capsys):
+    res = argv_parser.parse_multiple(["--test", "hi", "--key","world"], ["--test"])
+    assert "--test" in res
+    assert res["--test"] == "hi"
+    assert len(res.keys()) == 1
+
+def test_argv_parse_multiple_3(capsys):
+    happend = False
+    try:
+        argv_parser.parse_multiple(["--test", "hi", "--key","world"], ["--te2st", "--key"])
+    except NameError:
+        happend = True
+    assert happend
+
+def test_argv_parse_multiple_4(capsys):
+    happend = False
+    try:
+        argv_parser.parse_multiple(["--test", "hi", "--key",], ["--test", "--key"])
+    except ValueError:
+        happend = True
+    assert happend
