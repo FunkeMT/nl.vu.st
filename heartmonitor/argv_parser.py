fb39079cbab9587f0ef3e54961cbfe23ecea1246
pjotr <pjotrscholtze1@gmail.com>
2020-04-20 20:39:19 +0200

    Personal information should be given as command line arguments #8

--- heartmonitor/argv_parser.py
+++ heartmonitor/argv_parser.py
@@ -5,6 +5,8 @@ def parse(argv: List[str], key: str):
     :param: argv: Commandline arguments.
     :param: key: Key to find in the commandline arguments.
     :return: Value of the commandline argument.
+    :raises: NameError When key was not found.
+    :raises: ValueError When value was not found after key.
     """
     if key not in argv:
         raise NameError
