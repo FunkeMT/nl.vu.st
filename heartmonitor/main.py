546d5d12d1da8614bd7d23e9dc8db3b3621ad7ad
pjotr <pjotrscholtze1@gmail.com>
2020-04-23 11:53:53 +0200

    Personal information should be given as command line arguments #8

--- heartmonitor/main.py
+++ heartmonitor/main.py
@@ -2,33 +2,6 @@ import argv_parser, entity
 import sys
 from typing import List
 
-def get_patient_information(argv: List[str]) -> entity.Patient:
-    """
-    Get patient information from argv.
-
-    :param: argv: Argv to look into.
-    :return: Patient filled with commandline information.
-    :raises: NameError When key was not found.
-    :raises: ValueError When value was not found after key.
-    """
-
-    fields = ["age", "height", "weight", "gender"]
-    arguments = ["--" + name for name in fields]
-    raw_patient = argv_parser.parse_multiple(argv, arguments)
-    obj_arguments = {}
-
-    for field in ["age", "height", "weight"]:
-        obj_arguments[field] = int(raw_patient["--" + field])
-        if obj_arguments[field] < 0: raise ValueError
-    
-    gender = raw_patient["--gender"].lower()
-    if gender not in ["male", "female"]: raise ValueError
-
-    obj_arguments["gender"] = entity.GenderEnum.MALE
-    if gender == "female": obj_arguments["gender"] = entity.GenderEnum.FEMALE
-
-    return entity.Patient(**obj_arguments)
-
 
 def print_help():
     """
@@ -38,10 +11,6 @@ def print_help():
     print("heartbeatmonitor.py arguments:")
     print("")
     print("Arguments are the following:")
-    print("  --age number\tAge of the patient")
-    print("  --height whole-number\tHeight of the patient")
-    print("  --weight whole-number\tWeight of the patient")
-    print("  --gender text\tGender of the patient. Supported values (capital insensitve): male, female")
 
 def main(argv: List[str]):
     patient = None # type: entity.Patient
--- heartmonitor/tests/test_main.py
+++ heartmonitor/tests/test_main.py
@@ -3,68 +3,6 @@ import pytest
 import main
 
 
-def test_get_patient_information(capsys):
-    happend = False
-    try:
-        main.get_patient_information([])
-    except NameError:
-        happend = True
-    assert happend 
-
-def test_get_patient_information_2(capsys):
-    happend = False
-    try:
-        main.get_patient_information(["--age", "10", "--height", "10", "--weight", "10", "--gender"])
-    except ValueError:
-        happend = True
-    assert happend 
-
-
-def test_get_patient_information_3(capsys):
-    main.get_patient_information(["--age", "10", "--height", "10", "--weight", "10", "--gender", "male"])
-    assert True
-
-def test_get_patient_information_4(capsys):
-    happend = False
-    try:
-        main.get_patient_information(["--age", "-10", "--height", "10", "--weight", "10", "--gender", "male"])
-    except ValueError:
-        happend = True
-    
-    assert happend
-
-def test_get_patient_information_5(capsys):
-    happend = False
-    try:
-        main.get_patient_information(["--age", "10", "--height", "-10", "--weight", "10", "--gender", "male"])
-    except ValueError:
-        happend = True
-    
-    assert happend
-
-def test_get_patient_information_6(capsys):
-    happend = False
-    try:
-        main.get_patient_information(["--age", "10", "--height", "10", "--weight", "-10", "--gender", "male"])
-    except ValueError:
-        happend = True
-    
-    assert happend
-
-def test_get_patient_information_7(capsys):
-    happend = False
-    try:
-        main.get_patient_information(["--age", "10", "--height", "10", "--weight", "10", "--gender", "asd"])
-    except ValueError:
-        happend = True
-    
-    assert happend
-
-def test_get_patient_information_8(capsys):
-    main.get_patient_information(["--age", "10", "--height", "10", "--weight", "10", "--gender", "female"])
-    assert True
-
-
 def test_help(capsys):
     """Correct name argument and printing"""
     main.print_help()
@@ -74,7 +12,3 @@ def test_help(capsys):
     assert "heartbeatmonitor.py arguments:" == lines[1]
     assert "" == lines[2]
     assert "Arguments are the following:" == lines[3]
-    assert "  --age number\tAge of the patient" == lines[4]
-    assert "  --height whole-number\tHeight of the patient" == lines[5]
-    assert "  --weight whole-number\tWeight of the patient" == lines[6]
-    assert "  --gender text\tGender of the patient. Supported values (capital insensitve): male, female" == lines[7]
