0ff56e380ade4ce647f89c2eccae742b5be91157
Sven Preng <s.preng@student.vu.nl>
2020-04-24 15:34:59 +0200

    blood pressure checking works issue #14

--- heartmonitor/main.py
+++ heartmonitor/main.py
@@ -2,6 +2,7 @@ import argv_parser, entity, processor
 import sys
 from typing import List
 
+
 def print_help():
     """
     Print help message.
@@ -14,29 +15,33 @@ def print_help():
 
 
 def main(argv: List[str]):
-    csv_location = None # type: str
+    csv_location = None  # type: str
     try:
         csv_location = argv_parser.parse(argv, "--path")
     except:
         print_help()
         sys.exit(1)
 
-    #Create statistics
+    # Create statistics
     oxygen_ms = entity.MeasurementStatistics()
     pulse_ms = entity.MeasurementStatistics()
     blood_pressure_systolic_ms = entity.MeasurementStatistics()
     blood_pressure_diastolic_ms = entity.MeasurementStatistics()
-    statistics = entity.Statistics(oxygen_ms, pulse_ms, blood_pressure_systolic_ms, blood_pressure_diastolic_ms)
-    
+    statistics = entity.Statistics(
+        oxygen_ms, pulse_ms, blood_pressure_systolic_ms, blood_pressure_diastolic_ms
+    )
+
     number_of_measurements = 0
 
     for m in entity.FileRecording(csv_location).get_iterator():
         print(m.__dict__)
-        measurement_results = processor.processing_agent(m, statistics) # add other measurements statistics here
+        measurement_results = processor.processing_agent(
+            m, statistics
+        )  # add other measurements statistics here
         print("RESULT FOR OXYGEN", measurement_results.oxygen_status)
+        print("RESULT FOR SYSBP", measurement_results.blood_pressure_systolic_status)
+        print("RESULT FOR DIABP", measurement_results.blood_pressure_diastolic_status)
         number_of_measurements += 1
-
-
 
 
 if __name__ == "__main__":
--- heartmonitor/processor.py
+++ heartmonitor/processor.py
@@ -1,6 +1,7 @@
 import entity
 from typing import List
 
+
 def processing_agent(measurement: entity.Measurement, statistics: entity.Statistics):
     """
     The processing agent goes through the whole measurement. 
@@ -11,26 +12,34 @@ def processing_agent(measurement: entity.Measurement, statistics: entity.Statist
     those results will be returned to the main
     """
     oxygen_status = oxygen_analysis(measurement.oxygen, statistics.oxygen)
+    systolic_status = blood_pressure_systolic_analysis(
+        measurement.blood_pressure_systolic, statistics.blood_pressure_systolic
+    )
+    diastolic_status = blood_pressure_diastolic_analysis(
+        measurement.blood_pressure_diastolic, statistics.blood_pressure_diastolic
+    )
 
-    #pulse_status = process_pulse(pulse)
-    #systolic_status = process_blood_pressure_systolic(blood_pressure_systolic)
-    #diastolic_status = process_blood_pressure_diastolic(blood_pressure_diastolic)
+    # pulse_status = process_pulse(pulse)
 
     # Add new statuses to the result
-    measurement_results = entity.MeasurementResult(measurement, oxygen_status)
+    measurement_results = entity.MeasurementResult(
+        measurement, oxygen_status, None, systolic_status, diastolic_status
+    )
 
     return measurement_results
+
 
 def oxygen_analysis(oxygen, measurement_statistics):
     """
     Checks validation and valuation and increment status in statistics of Oxygen
     """
     if not oxygen_validation(oxygen):
-    	return entity.StatusEnum.MISSING
+        return entity.StatusEnum.MISSING
 
     status = oxygen_valuation(oxygen)
-    measurement_statistics.increment(status) 
+    measurement_statistics.increment(status)
     return status
+
 
 def oxygen_validation(oxygen: int):
     """
@@ -41,23 +50,97 @@ def oxygen_validation(oxygen: int):
 
     return int(oxygen) < 101 and int(oxygen) >= 0
 
+
 def oxygen_valuation(oxygen):
     """
     Checks different values to assign a status to
     """
-    if oxygen >= 95: return entity.StatusEnum.OK
-    elif oxygen >= 90: return entity.StatusEnum.MINOR
-    elif oxygen > 60: return entity.StatusEnum.MAJOR
-    elif oxygen <= 60: return entity.StatusEnum.LIFE_THREATENING
+    if oxygen >= 95:
+        return entity.StatusEnum.OK
+    elif oxygen >= 90:
+        return entity.StatusEnum.MINOR
+    elif oxygen > 60:
+        return entity.StatusEnum.MAJOR
+    elif oxygen <= 60:
+        return entity.StatusEnum.LIFE_THREATENING
+
 
 def process_pulse(pulse):
-	# To be implemented
+    # To be implemented
     pass
 
-def process_blood_pressure_systolic(blood_pressure_systolic):
-	# To be implemented
-    pass
 
-def process_blood_pressure_diastolic(blood_pressure_diastolic):
-	# To be implemented
-    pass
+def blood_pressure_systolic_analysis(blood_pressure_systolic, measurement_statistics):
+
+    if not blood_pressure_systolic_validation(blood_pressure_systolic):
+        return entity.StatusEnum.MISSING
+
+    status = blood_pressure_systolic_valuation(blood_pressure_systolic)
+    measurement_statistics.increment(status)
+    return status
+
+
+def blood_pressure_systolic_validation(blood_pressure_systolic):
+
+    if not str(blood_pressure_systolic).isnumeric():
+        return False
+
+    return int(blood_pressure_systolic) < 251 and int(blood_pressure_systolic) >= 0
+
+
+def blood_pressure_systolic_valuation(blood_pressure_systolic):
+    """
+    Checks different values to assign a status to
+    """
+    if blood_pressure_systolic >= 180:
+        return entity.StatusEnum.LIFE_THREATENING
+    elif blood_pressure_systolic < 40:
+        return entity.StatusEnum.LIFE_THREATENING
+    elif blood_pressure_systolic >= 140:
+        return entity.StatusEnum.MAJOR
+    elif blood_pressure_systolic < 60:
+        return entity.StatusEnum.MAJOR
+    elif blood_pressure_systolic > 130:
+        return entity.StatusEnum.MINOR
+    elif blood_pressure_systolic < 90:
+        return entity.StatusEnum.MINOR
+    elif blood_pressure_systolic <= 129:
+        return entity.StatusEnum.OK
+
+
+def blood_pressure_diastolic_analysis(blood_pressure_diastolic, measurement_statistics):
+
+    if not blood_pressure_diastolic_validation(blood_pressure_diastolic):
+        return entity.StatusEnum.MISSING
+
+    status = blood_pressure_diastolic_valuation(blood_pressure_diastolic)
+    measurement_statistics.increment(status)
+    return status
+
+
+def blood_pressure_diastolic_validation(blood_pressure_diastolic):
+
+    if not str(blood_pressure_diastolic).isnumeric():
+        return False
+
+    return int(blood_pressure_diastolic) < 141 and int(blood_pressure_diastolic) >= 0
+
+
+def blood_pressure_diastolic_valuation(blood_pressure_diastolic):
+    """
+    Checks different values to assign a status to
+    """
+    if blood_pressure_diastolic >= 120:
+        return entity.StatusEnum.LIFE_THREATENING
+    elif blood_pressure_diastolic < 40:
+        return entity.StatusEnum.LIFE_THREATENING
+    elif blood_pressure_diastolic >= 90:
+        return entity.StatusEnum.MAJOR
+    elif blood_pressure_diastolic < 50:
+        return entity.StatusEnum.MAJOR
+    elif blood_pressure_diastolic > 80:
+        return entity.StatusEnum.MINOR
+    elif blood_pressure_diastolic < 60:
+        return entity.StatusEnum.MINOR
+    elif blood_pressure_diastolic <= 80:
+        return entity.StatusEnum.OK
